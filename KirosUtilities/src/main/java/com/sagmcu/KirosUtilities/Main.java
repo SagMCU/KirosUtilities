c386e4cf88a3f377cbef02a5c0b2ace56f2eeabe
SagMCU <52503815+SagMCU@users.noreply.github.com>
2025-02-16 15:51:17 +0100

    Uploaded the whole code

---
+++ KirosUtilities/src/main/java/com/sagmcu/KirosUtilities/Main.java
@@ -0,0 +1,38 @@
+package com.sagmcu.KirosUtilities;
+
+import com.sagmcu.KirosUtilities.mechanisms.PistonBlocker;
+import com.sagmcu.KirosUtilities.commands.ReloadCommand;
+import com.sagmcu.KirosUtilities.utils.ChatColorTranslator;
+import com.sagmcu.KirosUtilities.utils.TabCompletionFilter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    private PistonBlocker pistonBlocker;
+    private TabCompletionFilter tabCompletionFilter;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        pistonBlocker = new PistonBlocker(this);
+        getServer().getPluginManager().registerEvents(pistonBlocker, this);
+        tabCompletionFilter = new TabCompletionFilter(this);
+        getServer().getPluginManager().registerEvents(tabCompletionFilter, this);
+        getCommand("kirosutilities").setExecutor(new ReloadCommand(this));
+        String enabledMessage = ChatColorTranslator.translate("&aKirosUtilities &7has been &aenabled&7! &6:D");
+        this.getServer().getConsoleSender().sendMessage(enabledMessage);
+    }
+
+    @Override
+    public void onDisable() {
+        String disabledMessage = ChatColorTranslator.translate("&cKirosUtilities &7has been &cdisabled&7! :(");
+        this.getServer().getConsoleSender().sendMessage(disabledMessage);
+    }
+
+    public PistonBlocker getPistonBlocker() {
+        return pistonBlocker;
+    }
+
+    public TabCompletionFilter getTabCompletionFilter() {
+        return tabCompletionFilter;
+    }
+}
---
+++ KirosUtilities/src/main/java/com/sagmcu/KirosUtilities/commands/ReloadCommand.java
@@ -0,0 +1,45 @@
+package com.sagmcu.KirosUtilities.commands;
+
+import com.sagmcu.KirosUtilities.Main;
+import com.sagmcu.KirosUtilities.utils.ChatColorTranslator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ReloadCommand implements CommandExecutor {
+    private final Main plugin;
+
+    public ReloadCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        String reloadSuccessMessage = plugin.getConfig().getString("messages.reload_success");
+        String noPermissionMessage = plugin.getConfig().getString("messages.no_permission");
+
+        reloadSuccessMessage = ChatColorTranslator.translate(reloadSuccessMessage);
+        noPermissionMessage = ChatColorTranslator.translate(noPermissionMessage);
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("kirosutilities.admin")) {
+            player.sendMessage(noPermissionMessage);
+            return false;
+        }
+
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            plugin.reloadConfig();
+            plugin.getPistonBlocker().reloadBlockedMaterials();
+            if (plugin.getTabCompletionFilter() != null) {
+                plugin.getTabCompletionFilter().reloadConfig();
+            }
+            player.sendMessage(reloadSuccessMessage);
+            return true;
+        }
+
+        return false;
+    }
+}
