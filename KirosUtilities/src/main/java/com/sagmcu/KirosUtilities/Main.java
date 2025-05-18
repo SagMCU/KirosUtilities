750e542e52825b852e82590226b05604e14b8cc4
SagMCU <52503815+SagMCU@users.noreply.github.com>
2025-05-18 05:25:13 +0200

    Moved TabCompletionFilter and added JoinFullServers

--- KirosUtilities/src/main/java/com/sagmcu/KirosUtilities/Main.java
+++ KirosUtilities/src/main/java/com/sagmcu/KirosUtilities/Main.java
@@ -2,8 +2,10 @@ package com.sagmcu.KirosUtilities;
 
 import com.sagmcu.KirosUtilities.mechanisms.PistonBlocker;
 import com.sagmcu.KirosUtilities.commands.ReloadCommand;
+import com.sagmcu.KirosUtilities.filters.TabCompletionFilter;
+import com.sagmcu.KirosUtilities.handlers.JoinFullServers;
 import com.sagmcu.KirosUtilities.utils.ChatColorTranslator;
-import com.sagmcu.KirosUtilities.utils.TabCompletionFilter;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -18,6 +20,8 @@ public class Main extends JavaPlugin {
         tabCompletionFilter = new TabCompletionFilter(this);
         getServer().getPluginManager().registerEvents(tabCompletionFilter, this);
         getCommand("kirosutilities").setExecutor(new ReloadCommand(this));
+        // Register JoinFullServers listener
+        getServer().getPluginManager().registerEvents(new JoinFullServers(), this);
         String enabledMessage = ChatColorTranslator.translate("&aKirosUtilities &7has been &aenabled&7! &6:D");
         this.getServer().getConsoleSender().sendMessage(enabledMessage);
     }
